d33bcb0898fbc4b654efb12fde4cfa745be69994
Rajat Patwa <razat249@gmail.com>
2017-07-29 13:37:54 +0530

    add react router dom

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import IssuesPageContainer from '../IssuesPageContainer/IssuesPageContainer';
 import RegionsPageContainer from '../RegionsPageContainer/RegionsPageContainer';
@@ -8,8 +9,12 @@ export class Layout extends Component {
   render() {
     return (
       <div className="container">
-        <RegionsPageContainer />
-        <IssuesPageContainer />
+        <Router>
+          <div>
+            <Route exact path="/" component={RegionsPageContainer}/>
+            <Route path="/issues/:id" component={IssuesPageContainer}/>
+          </div>
+        </Router>
       </div>
     );
   }
